3008ea54bde997a5195953f1111bc98763620edc
winfred <winfred@hired.com>
2014-11-13 22:36:40 -0800

    provide injection pathway for theme intents and components + host rails apps can customize their color-space + host rails apps can select specific components to document + host rails apps can also provide their own documentation to new components or override as needed

--- app/helpers/fortitude_rails/application_helper.rb
+++ app/helpers/fortitude_rails/application_helper.rb
@@ -1,4 +1,11 @@
 module FortitudeRails
   module ApplicationHelper
+    def theme_intents
+      FortitudeRails.theme_intents
+    end
+
+    def theme_components
+      FortitudeRails.theme_components
+    end
   end
 end
--- lib/fortitude_rails.rb
+++ lib/fortitude_rails.rb
@@ -1,4 +1,49 @@
 require "fortitude_rails/engine"
 
 module FortitudeRails
+  mattr_accessor :theme_intents
+  mattr_accessor :theme_components
+
+  def self.theme_intents
+    @@theme_intents ||= %w(
+      default
+      primary
+      success
+      info
+      warning
+      danger
+    )
+  end
+
+  def self.theme_components
+    @@theme_components ||= %w(
+      badge
+      bare-list
+      block-list
+      box
+      button
+      container
+      element
+      flag
+      flashbar
+      fluid-container
+      form
+      inline-list
+      input
+      layout
+      list-navigation
+      media
+      modal
+      navigationbar
+      shade
+      table
+      tabs
+      tabs-navigation
+      tooltip
+      typography
+      ui-list
+      wings
+      utilities
+    )
+  end
 end
